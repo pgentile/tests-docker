bd908824494b598edf4113cbfc51b8fb90a50ea1
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-11-18 21:14:17 +0100

    Meilleur respect des conventions Linux

--- consul/entrypoint.py
+++ consul/entrypoint.py
@@ -11,7 +11,7 @@ user = pwd.getpwnam("consul")
 
 # Modifier les doits sur le répertoire des données
 
-os.chown("/var/local/consul", user.pw_uid, user.pw_gid)
+os.chown("/var/consul", user.pw_uid, user.pw_gid)
 
 
 # Lancer Consul en droppant les privilèges root
@@ -20,6 +20,6 @@ os.setgroups([user.pw_gid])
 os.setgid(user.pw_gid)
 os.setuid(user.pw_uid)
 
-consul_bin = "/usr/local/bin/consul"
-args = [consul_bin, "agent", "-config-dir", "/usr/local/etc/consul.d"] + sys.argv[1:]
+consul_bin = "/usr/bin/consul"
+args = [consul_bin, "agent", "-config-dir", "/etc/consul.d"] + sys.argv[1:]
 os.execv(consul_bin, args)
