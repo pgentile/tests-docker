120235f615fcc3c7ce8858b4b69d4f92b8de2e3b
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-11-27 19:42:35 +0100

    Pas de saut de ligne dans le fichier requirements.txt

--- python-wheel-onbuild/build-wheels.sh
+++ python-wheel-onbuild/build-wheels.sh
@@ -2,7 +2,7 @@
 
 pip wheel --wheel-dir /var/local/wheels/ $@
 
-echo >/var/local/wheels/requirements.txt
+rm -f /var/local/wheels/requirements.txt
 for name in $@
 do
     echo $name >>/var/local/wheels/requirements.txt
