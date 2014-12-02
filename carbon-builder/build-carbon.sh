413574d2e30e39ab1b4adbf15c3d49eebb62ce54
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-12-02 20:41:05 +0100

    Construction des Wheels et des dépendances nécessaires pour Carbon, construction de l'image Graphite-API avec les dépendances vers Carbon

--- carbon-builder/build-carbon.sh
+++ carbon-builder/build-carbon.sh
@@ -6,3 +6,11 @@ python2.7 setup.py bdist_wheel
 mv dist/*.whl /var/local/wheels/
 
 echo carbon >/var/local/wheels/requirements.txt
+
+requirements='Twisted==11.1.0 txamqp whisper==0.9.12'
+for requirement in $requirements
+do
+    echo "$requirement" >>/var/local/wheels/requirements.txt
+done
+
+pip wheel --wheel-dir /var/local/wheels/ $requirements
