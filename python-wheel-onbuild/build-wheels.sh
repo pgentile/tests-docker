b108b51d05bf7bfdb2b6dee204a51f9c10b72dd2
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-12-02 20:13:50 +0100

    Amélioration du script qui génère les outputs

--- python-wheel-onbuild/build-wheels.sh
+++ python-wheel-onbuild/build-wheels.sh
@@ -3,13 +3,22 @@
 pip wheel --wheel-dir /var/local/wheels/ $@
 
 rm -f /var/local/wheels/requirements.txt
-for name in $@
+while [[ -n "$1 " ]]
 do
-    echo $name >>/var/local/wheels/requirements.txt
+    case "$1" in
+        -*)
+            echo "$1 $2" >>/var/local/wheels/requirements.txt
+            shift 2
+            ;;
+        *)
+            echo "$1" >>/var/local/wheels/requirements.txt
+            shift
+            ;;
+    esac
 done
 
 cat <<EOF
 Pour installer les packages Whell generes :
 
-  pip install -r @WHEEL_DIR@/requirements.txt --find-links @WHEEL_DIR@ --no-index
+    pip install -r @WHEEL_DIR@/requirements.txt --find-links @WHEEL_DIR@ --no-index
 EOF
