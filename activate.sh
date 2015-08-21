47b450946797d4a7bc211003c7ae29752916b622
Pierre Gentile <pierre.gentile.perso@gmail.com>
2015-08-21 21:59:54 +0200

    Passage vers docker-machine

--- activate.sh
+++ activate.sh
@@ -1,8 +1,4 @@
-boot2docker up
+export DOCKER_MACHINE_NAME=default
 
-$(boot2docker shellinit)
-
-# Surcharger l'IP avec le hostname boot2docker défini dans /etc/hosts pour ne pas se faire avoir
-# avec la vérification SSL des hostnames par fig
-boot2docker_ip=$(boot2docker ip)
-export DOCKER_HOST=$(echo "$DOCKER_HOST" | sed "s|tcp://$boot2docker_ip|tcp://boot2docker|")
+docker-machine start $DOCKER_MACHINE_NAME
+eval $(docker-machine env $DOCKER_MACHINE_NAME)
