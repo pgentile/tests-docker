f81e8343992b014859177b3c172309a64ea6fa45
Pierre Gentile <pierre.gentile.perso@gmail.com>
2015-08-30 09:50:42 +0200

    Construction package Consul

---
+++ fpm-consul/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+exec fpm \
+    -f \
+    -s dir \
+    -t deb \
+    --name consul \
+    --version $CONSUL_VERSION \
+    --license 'Mozilla Public License 2.0' \
+    --category net \
+    --url 'http://consul.io' \
+    --description 'Consul - Service discovery and configuration made easy' \
+    --config-files /lib/systemd/system/ \
+    --config-files /etc/consul.d/ \
+    --after-install scripts/after-install.sh \
+    consul=/usr/bin/ \
+    config/systemd/consul.service=/lib/systemd/system/ \
+    config/consul/logging.json=/etc/consul.d/
