eee6931fc809123756544b7bcc9ba08cfa9c98e4
Pierre Gentile <pierre.gentile.perso@gmail.com>
2016-09-15 20:25:45 +0200

    Config simple d'un cluster Redis

--- redis-cluster/redis-cluster-entrypoint.sh
+++ redis-cluster/redis-cluster-entrypoint.sh
@@ -6,12 +6,20 @@ cat >/etc/redis.conf <<-EOF
 timeout 600
 tcp-keepalive 60
 loglevel notice
-databases 1
-appendonly yes
 
 cluster-enabled yes
 cluster-config-file redis-cluster.conf
 cluster-node-timeout 5000
 EOF
 
-exec /usr/local/bin/docker-entrypoint.sh redis-server /etc/redis.conf "$@"
+create_cluster_file=/usr/local/bin/create-cluster.sh
+
+cat >$create_cluster_file <<-EOF
+#!/bin/bash
+nodes=\$(getent hosts redis01 redis02 redis03 redis04 redis05 redis06 | awk '{ print \$1 ":6379" }')
+exec redis-trib.rb create --replicas 1 \$nodes
+EOF
+
+chmod +x $create_cluster_file
+
+exec docker-entrypoint.sh redis-server /etc/redis.conf "$@"
